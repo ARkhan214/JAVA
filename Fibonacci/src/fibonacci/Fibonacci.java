789a4110bebae9c32debb38fb1a4c8a1cac146bb
ARkhan214 <abdurrahimkhan214dc@gmail.com>
2025-04-17 19:04:24 +0600

    add

---
+++ Fibonacci/src/fibonacci/Fibonacci.java
@@ -0,0 +1,28 @@
+
+package fibonacci;
+
+import java.util.Scanner;
+
+
+public class Fibonacci {
+
+    
+    public static void main(String[] args) {
+       Scanner s=new Scanner(System.in);
+        
+        System.out.println("Enter number");
+        int size=s.nextInt();
+        
+        
+        int number1=0;
+        int number2=1;
+        int sum=0;
+        System.out.println(number1);
+        System.out.println(number2);
+        
+        for(){
+        
+        }
+    }
+    
+}
---
+++ MultiDaimantionArrays/src/multidaimantionarrays/MultiDaimantionArrays.java
@@ -0,0 +1,29 @@
+
+package multidaimantionarrays;
+
+import java.util.Arrays;
+
+
+public class MultiDaimantionArrays {
+
+    
+    public static void main(String[] args) {
+        
+        int myArrays[][]=new int[2][3];
+        
+        System.out.println(Arrays.deepToString(myArrays));
+        
+        myArrays[0][1]=5;
+        myArrays[1][1]=8;
+        myArrays[1][0]=8;
+        System.out.println(Arrays.deepToString(myArrays));
+        
+        for(int row=0;row<myArrays.length;row++){
+            for(int col=0;col<myArrays[row].length;col++){
+                System.out.println(myArrays[row][col]+" ");
+            }
+            System.out.println("\n");
+        }
+    }
+    
+}
