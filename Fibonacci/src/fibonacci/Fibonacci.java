1ca40da629d96987c2b08fb98fb990509f4d8000
ARkhan214 <abdurrahimkhan214dc@gmail.com>
2025-04-21 00:42:42 +0600

    Java Evidence

--- Fibonacci/src/fibonacci/Fibonacci.java
+++ Fibonacci/src/fibonacci/Fibonacci.java
@@ -8,11 +8,28 @@ public class Fibonacci {
 
     
     public static void main(String[] args) {
-       Scanner s=new Scanner(System.in);
+//       Scanner s=new Scanner(System.in);
+//        
+//        System.out.println("Enter number");
+//        int size=s.nextInt();
+//        
+//        
+//        int number1=0;
+//        int number2=1;
+//        int sum=0;
+//        System.out.println(number1);
+//        System.out.println(number2);
+//        
+//        for(){
+//        
+//        }
         
-        System.out.println("Enter number");
-        int size=s.nextInt();
         
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter limit :");
+        int size = s.nextInt();
+        System.out.println("-------------------------------");
         
         int number1=0;
         int number2=1;
@@ -20,9 +37,16 @@ public class Fibonacci {
         System.out.println(number1);
         System.out.println(number2);
         
-        for(){
+        for(int i=3; i<=size; i++){
+            sum= number1+number2;
+            System.out.println(sum);
+            
+            number1 = number2;
+            number2 = sum;        
+        } 
+//        System.out.println(sum);
         
-        }
+        
     }
     
 }
